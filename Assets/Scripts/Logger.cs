d1fc40748146a8840e14209c16aabf16d7ae7745
adcimon <37084114+adcimon@users.noreply.github.com>
2021-10-26 18:10:32 +0200

    First commit

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Logger : MonoBehaviour
+{
+    private Text text;
+
+    private void Awake()
+    {
+        text = this.transform.GetChild(0).GetComponent<Text>();
+    }
+
+    public void Info( string message )
+    {
+        text.text = message;
+    }
+
+    public void Error( string message )
+    {
+        text.text = "<color=red>" + message + "</color>";
+    }
+}
