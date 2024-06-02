d0616608b5d20d801752214ec98d4225ead171d1
adcimon <37084114+adcimon@users.noreply.github.com>
2024-06-02 16:37:57 +0200

    Updated formatting

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -3,20 +3,20 @@ using UnityEngine.UI;
 
 public class Logger : MonoBehaviour
 {
-    private Text text;
+	private Text text;
 
-    private void Awake()
-    {
-        text = this.transform.GetChild(0).GetComponent<Text>();
-    }
+	private void Awake()
+	{
+		text = this.transform.GetChild(0).GetComponent<Text>();
+	}
 
-    public void Info( string message )
-    {
-        text.text = message;
-    }
+	public void Info(string message)
+	{
+		text.text = message;
+	}
 
-    public void Error( string message )
-    {
-        text.text = "<color=red>" + message + "</color>";
-    }
+	public void Error(string message)
+	{
+		text.text = "<color=red>" + message + "</color>";
+	}
 }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -21,100 +21,100 @@ using Unity.Collections;
 
 public class Main : MonoBehaviour
 {
-    public AROcclusionManager occlusionManager;
-    public Material blitMaterial;
-    public Logger logger;
-    public UnityEvent<Texture2D> onDepthTexture;
+	public AROcclusionManager occlusionManager;
+	public Material blitMaterial;
+	public Logger logger;
+	public UnityEvent<Texture2D> onDepthTexture;
 
-    private bool initialized = false;
-    private Texture2D depthTexture;
+	private bool initialized = false;
+	private Texture2D depthTexture;
 
-    private void Start()
-    {
-        StartCoroutine(Initialize());
-    }
+	private void Start()
+	{
+		StartCoroutine(Initialize());
+	}
 
-    private void Update()
-    {
-        if( !initialized )
-        {
-            return;
-        }
+	private void Update()
+	{
+		if (!initialized)
+		{
+			return;
+		}
 
-        XRCpuImage cpuImage;
-        if( occlusionManager && occlusionManager.TryAcquireEnvironmentDepthCpuImage(out cpuImage) )
-        {
-            using( cpuImage )
-            {
-                bool recreated = UpdateDepthTexture(ref depthTexture, cpuImage);
-                if( recreated )
-                {
-                    onDepthTexture?.Invoke(depthTexture);
-                }
+		XRCpuImage cpuImage;
+		if (occlusionManager && occlusionManager.TryAcquireEnvironmentDepthCpuImage(out cpuImage))
+		{
+			using (cpuImage)
+			{
+				bool recreated = UpdateDepthTexture(ref depthTexture, cpuImage);
+				if (recreated)
+				{
+					onDepthTexture?.Invoke(depthTexture);
+				}
 
-                LogInfo();
+				LogInfo();
 
-                cpuImage.Dispose();
-            }
-        }
-    }
+				cpuImage.Dispose();
+			}
+		}
+	}
 
-    private IEnumerator Initialize()
-    {
-        yield return new WaitForSeconds(3);
+	private IEnumerator Initialize()
+	{
+		yield return new WaitForSeconds(3);
 
-        // Check if AR is supported.
-        if( occlusionManager == null || occlusionManager.descriptor == null )
-        {
-            logger.Error("Occlusion manager or descriptor is null");
-            yield return null;
-        }
+		// Check if AR is supported.
+		if (occlusionManager == null || occlusionManager.descriptor == null)
+		{
+			logger.Error("Occlusion manager or descriptor is null");
+			yield return null;
+		}
 
-        LogInfo();
+		LogInfo();
 
-        initialized = true;
-    }
+		initialized = true;
+	}
 
-    private bool UpdateDepthTexture( ref Texture2D texture, XRCpuImage cpuImage )
-    {
-        bool recreated = false;
+	private bool UpdateDepthTexture(ref Texture2D texture, XRCpuImage cpuImage)
+	{
+		bool recreated = false;
 
-        // If the texture hasn't yet been created, or if its dimensions or format have changed, (re)create the texture.
-        // Although texture dimensions do not normally change frame-to-frame, they can change in response to a change in the camera resolution (for camera images)
-        // or changes to the quality of the human depth and human stencil buffers.
-        if( (texture == null) || (texture.width != cpuImage.width) || (texture.height != cpuImage.height) || (texture.format != cpuImage.format.AsTextureFormat()) )
-        {
-            texture = new Texture2D(cpuImage.width, cpuImage.height, cpuImage.format.AsTextureFormat(), false);
-            recreated = true;
-        }
+		// If the texture hasn't yet been created, or if its dimensions or format have changed, (re)create the texture.
+		// Although texture dimensions do not normally change frame-to-frame, they can change in response to a change in the camera resolution (for camera images)
+		// or changes to the quality of the human depth and human stencil buffers.
+		if ((texture == null) || (texture.width != cpuImage.width) || (texture.height != cpuImage.height) || (texture.format != cpuImage.format.AsTextureFormat()))
+		{
+			texture = new Texture2D(cpuImage.width, cpuImage.height, cpuImage.format.AsTextureFormat(), false);
+			recreated = true;
+		}
 
-        // Mirror about the horizontal axis.
-        XRCpuImage.ConversionParams conversionParams = new XRCpuImage.ConversionParams(cpuImage, cpuImage.format.AsTextureFormat(), XRCpuImage.Transformation.MirrorX);
+		// Mirror about the horizontal axis.
+		XRCpuImage.ConversionParams conversionParams = new XRCpuImage.ConversionParams(cpuImage, cpuImage.format.AsTextureFormat(), XRCpuImage.Transformation.MirrorX);
 
-        // Get the texture's underlying pixel data.
-        NativeArray<byte> data = depthTexture.GetRawTextureData<byte>();
+		// Get the texture's underlying pixel data.
+		NativeArray<byte> data = depthTexture.GetRawTextureData<byte>();
 
-        // Make sure the pixel data is the same size as the converted data.
-        if( data != null && data.Length == cpuImage.GetConvertedDataSize(conversionParams.outputDimensions, conversionParams.outputFormat) )
-        {
-            cpuImage.Convert(conversionParams, data);
-            texture.Apply();
-            data.Dispose();
-        }
+		// Make sure the pixel data is the same size as the converted data.
+		if (data != null && data.Length == cpuImage.GetConvertedDataSize(conversionParams.outputDimensions, conversionParams.outputFormat))
+		{
+			cpuImage.Convert(conversionParams, data);
+			texture.Apply();
+			data.Dispose();
+		}
 
-        return recreated;
-    }
+		return recreated;
+	}
 
-    private void LogInfo()
-    {
-        string message = "";
+	private void LogInfo()
+	{
+		string message = "";
 
-        message += "Environment depth image: " + occlusionManager?.descriptor?.supportsEnvironmentDepthImage + "\n";
-        message += "Environment depth confidence image: " + occlusionManager?.descriptor?.supportsEnvironmentDepthConfidenceImage + "\n";
-        message += "Environment depth mode: " + occlusionManager?.currentEnvironmentDepthMode.ToString() + "\n";
-        message += "Occlusion preference mode: " + occlusionManager?.currentOcclusionPreferenceMode.ToString() + "\n";
-        message += "Depth texture: " + ((depthTexture == null) ? "0x0" : (depthTexture?.width + "x" + depthTexture?.height)) + "\n";
+		message += "Environment depth image: " + occlusionManager?.descriptor?.supportsEnvironmentDepthImage + "\n";
+		message += "Environment depth confidence image: " + occlusionManager?.descriptor?.supportsEnvironmentDepthConfidenceImage + "\n";
+		message += "Environment depth mode: " + occlusionManager?.currentEnvironmentDepthMode.ToString() + "\n";
+		message += "Occlusion preference mode: " + occlusionManager?.currentOcclusionPreferenceMode.ToString() + "\n";
+		message += "Depth texture: " + ((depthTexture == null) ? "0x0" : (depthTexture?.width + "x" + depthTexture?.height)) + "\n";
 
-        logger.Info(message);
-    }
+		logger.Info(message);
+	}
 }
